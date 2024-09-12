73d9207fc4349fb8d867593fc4f991b0eb9bcc54
ferthomaz <152456030+ferthomaz@users.noreply.github.com>
2024-09-11 22:01:48 -0300

    Add files via upload

---
+++ receitaaaaaas/src/dominio/Ingrediente.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dominio;
+
+/**
+ *
+ * @author heito
+ */
+public class Ingrediente {
+    private String nome;
+    private String tipo;
+
+    public Ingrediente(){}
+    public Ingrediente(String nome, String tipo) {
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    public boolean comparaIngrediente(Ingrediente ingrediente){
+        return this.nome.equals(ingrediente.getNome())&&this.tipo.equals(ingrediente.getTipo());
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    
+    
+}
